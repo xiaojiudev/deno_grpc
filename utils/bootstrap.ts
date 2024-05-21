07799bddee91873e5ef8870a64eb07df92501e8a
ldphat99 <ldphat99@gmail.com>
2024-05-21 10:16:56 +0700

    feat: load enviroment variables from yaml

---
+++ utils/bootstrap.ts
@@ -0,0 +1,17 @@
+import { parse } from 'npm:yaml';
+
+function loadLocalConfig(): Record<string, unknown> {
+    const yamlData = Deno.readTextFileSync("./local.yaml");
+    return parse(yamlData) as Record<string, unknown>;
+}
+
+const config = loadLocalConfig();
+
+const env = config.env as { name: string; value: string | number }[];
+
+export const APP_GRPC_PORT: number = env.find((e) => e.name === "APP_GRPC_PORT")?.value as number;
+
+export const APP_DATABASE_URI: string = env.find((e) => e.name === "APP_DATASOURCE_URI")?.value as string;
+
+export const APP_DATABASE_NAME: string = env.find((e) => e.name === "APP_DATABASE_NAME")?.value as string;
+
