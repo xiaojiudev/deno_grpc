0b185712db3fc2bfd5afdfc4e2e2ef6d420501c2
ldphat99 <ldphat99@gmail.com>
2024-05-21 10:14:25 +0700

    feat: initial dependencies

---
+++ deps.ts
@@ -0,0 +1,9 @@
+export { Hono } from 'https://deno.land/x/hono@v4.3.7/mod.ts';
+
+export { GrpcServer } from "https://deno.land/x/grpc_basic@0.4.7/server.ts";
+
+export { getClient } from "https://deno.land/x/grpc_basic@0.4.7/client.ts";
+
+export { MongoClient, Database } from "https://deno.land/x/mongo@v0.33.0/mod.ts";
+
+export { ObjectId, Collection } from "https://deno.land/x/mongo@v0.33.0/mod.ts";
