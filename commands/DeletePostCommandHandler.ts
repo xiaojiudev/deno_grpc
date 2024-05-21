c96b8b5d51c691215a7828549a2ffe0cb792c348
ldphat99 <ldphat99@gmail.com>
2024-05-21 11:46:40 +0700

    feat: delete post command

---
+++ commands/DeletePostCommandHandler.ts
@@ -0,0 +1,24 @@
+import { Bson, PostRequest, PostResponse, getPostsCollection } from "../deps.ts";
+
+export class DeletePostCommandHandler {
+    async handle(post: PostRequest): Promise<PostResponse> {
+        if (!post._id || !Bson.ObjectId.isValid(post._id.toString())) {
+            console.log("Not a valid ID");
+            return { success: false };
+        }
+
+        const postId = new Bson.ObjectId(post._id);
+        const PostCollection = await getPostsCollection();
+
+        const filter = { _id: postId};
+
+        const deleteCount = await PostCollection.deleteOne(filter);
+
+        if (deleteCount === 0) {
+            console.log("matched", deleteCount);
+            return { success: false }
+        }
+
+        return { success: true };
+    }
+}
