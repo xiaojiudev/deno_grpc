a92a3fa2107836b46768aebb42ddd41cbfb5f92a
ldphat99 <ldphat99@gmail.com>
2024-05-22 13:16:47 +0700

    build: change mongodb version to v0.32.0
    v0.32.0 has Bson method to check a valid ObjectId

--- deps.ts
+++ deps.ts
@@ -4,6 +4,15 @@ export { GrpcServer } from "https://deno.land/x/grpc_basic@0.4.7/server.ts";
 
 export { getClient } from "https://deno.land/x/grpc_basic@0.4.7/client.ts";
 
-export { MongoClient, Database } from "https://deno.land/x/mongo@v0.33.0/mod.ts";
+export { GrpcError, Status as GrpcStatus, error as GrpcException } from "https://deno.land/x/grpc_basic@0.4.7/error.ts";
 
-export { ObjectId, Collection } from "https://deno.land/x/mongo@v0.33.0/mod.ts";
+export { MongoClient, Database } from "https://deno.land/x/mongo@v0.32.0/mod.ts";
+
+export { Bson, ObjectId, Collection } from "https://deno.land/x/mongo@v0.32.0/mod.ts";
+
+export * from "./types/social_media.d.ts";
+
+export * from "./model/db.ts";
+
+export * from "./utils/bootstrap.ts";
+
