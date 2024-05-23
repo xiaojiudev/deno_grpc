9adf930697828c5e9c22d6db645f33ebe748912d
ldphat99 <ldphat99@gmail.com>
2024-05-23 17:14:33 +0700

    refactor: add missing properties

--- commands/CreatePostCommandHandler.ts
+++ commands/CreatePostCommandHandler.ts
@@ -1,16 +1,26 @@
-import { PostSchema } from "../deps.ts";
-import { Post, PostResponse, getPostsCollection } from "../deps.ts";
+import { Post, PostResponse, } from "../deps.ts";
+import { PostSchema, getPostsCollection } from "../model/PostSchema.ts";
 
 export class CreatePostCommandHandler {
     async handle(post: Post): Promise<PostResponse> {
         const PostCollection = await getPostsCollection();
 
         const { title, content, categories } = post;
-        
+
         const payload: PostSchema = {
             title,
             content,
             categories,
+            interactions: {
+                likes: 0,
+                comments: 0,
+                shares: 0,
+                clicked: 0,
+                profileClicked: 0,
+                bookmarked: 0,
+                photoExpanded: 0,
+                videoPlayback: 0,
+            },
             createdAt: new Date(),
             updatedAt: new Date(),
         }
