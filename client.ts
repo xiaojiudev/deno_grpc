8ddc06a4cf415d14a49c0574fafcee037d76e2a7
ldphat99 <ldphat99@gmail.com>
2024-05-30 15:20:14 +0700

    refactor: test keyword service in client

--- client.ts
+++ client.ts
@@ -1,17 +1,37 @@
-import { SocialMediaService, getClient } from "./deps.ts";
-const protoPath = new URL("./protos/social_media.proto", import.meta.url);
-const protoFile = await Deno.readTextFile(protoPath);
+import { KeywordService, SocialMediaService, getClient } from "./deps.ts";
+const postProtoPath = new URL("./protos/social_media.proto", import.meta.url);
+const postProtoFile = await Deno.readTextFile(postProtoPath);
 
-const client = getClient<SocialMediaService>({
+const keywordProtoPath = new URL("./protos/keyword.proto", import.meta.url);
+const keywordProtoFile = await Deno.readTextFile(keywordProtoPath);
+
+const clientA = getClient<SocialMediaService>({
     port: 50051,
-    root: protoFile,
+    root: postProtoFile,
     serviceName: "SocialMediaService",
 });
 
-await client
-    .ListPost({})
-    .then(data => console.log("All posts", data));
+const clientB = getClient<KeywordService>({
+    port: 50051,
+    root: keywordProtoFile,
+    serviceName: "KeywordService",
+});
 
-await client
-    .ListTrendingPosts({})
-    .then(data => console.log("Top 10 trending posts", data));
+// await clientA
+//     .ListPost({})
+//     .then(data => console.log("All posts", data));
+
+// await clientA
+//     .ListTrendingPosts({})
+//     .then(data => console.log("Top 10 trending posts", data));
+
+// await clientA
+//     .SearchPost({ search: "Category B" })
+//     .then(data => console.log("Search results", data));
+
+await clientB
+    .GetTopKeywords({})
+    .then(data => console.log("Top 10 hot keyword", data));
+
+clientA.close();
+clientB.close();
