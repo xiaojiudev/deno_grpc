1c05d58853521b4505ed633108e1460ab8fa0f1f
ldphat99 <ldphat99@gmail.com>
2024-05-31 11:41:53 +0700

    build: add testing dependencies

--- deps.ts
+++ deps.ts
@@ -10,9 +10,21 @@ export { MongoClient, Database } from "https://deno.land/x/mongo@v0.32.0/mod.ts"
 
 export { Bson, ObjectId, Collection } from "https://deno.land/x/mongo@v0.32.0/mod.ts";
 
+export * as bcrypt from "https://deno.land/x/bcrypt@v0.4.1/mod.ts";
+
+export { beforeEach, afterEach, describe, it } from "https://deno.land/std@0.224.0/testing/bdd.ts";
+
+export { assertArrayIncludes, assertRejects } from "https://deno.land/std@0.224.0/testing/asserts.ts";
+
+export { assertEquals } from "https://deno.land/std@0.224.0/assert/assert_equals.ts";
+
+export { assertObjectMatch } from "https://deno.land/std@0.224.0/assert/assert_object_match.ts";
+
 export * from "./types/social_media.d.ts";
 
 export * from "./types/keyword.d.ts";
+
+export * from "./types/user.d.ts";
 
 export * from "./utils/bootstrap.ts";
 
