33443ef3a35c863f1351629147fdb0d3cde20721
ldphat99 <ldphat99@gmail.com>
2024-05-31 11:40:06 +0700

    feat: define user services

---
+++ services/UserService.ts
@@ -0,0 +1,21 @@
+import { CreateUserCommandHandler } from '../commands/CreateUserCommandHandler.ts';
+import { getGrpcServer } from '../db/grpc.ts';
+import { UserService, CreateUserRequest, CreateUserResponse, EmptyRequest, PostRecommendationResponse } from '../deps.ts';
+
+export const initUserService = async () => {
+    const grpcServer = await getGrpcServer();
+
+    const userProtoPath = new URL("../protos/user.proto", import.meta.url);
+    const userProtoFile = Deno.readTextFileSync(userProtoPath);
+
+
+    grpcServer.addService<UserService>(userProtoFile, {
+        CreateUser: async (request: CreateUserRequest): Promise<CreateUserResponse> => {
+            const command = new CreateUserCommandHandler();
+            return await command.handle(request);
+        },
+        RecommendPosts: async (request: EmptyRequest): Promise<PostRecommendationResponse> => {
+            return { postId: ["id123", "id456"] }
+        }
+    });
+}
