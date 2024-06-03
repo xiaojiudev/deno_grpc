fa005ce003c8d20bc69e388a184f247e2d2bc057
ldphat99 <ldphat99@gmail.com>
2024-06-03 15:35:15 +0700

    refactor: add userId field in PostSchema

--- model/PostSchema.ts
+++ model/PostSchema.ts
@@ -1,36 +1,37 @@
 import { Bson, Collection } from "../deps.ts";
-import { getDB } from '../db/mongodb.ts';
+import { getDB } from "../db/mongodb.ts";
 
 export interface PostSchema {
-    _id?: Bson.ObjectId;
-    title: string
-    content: string
-    categories: string[]
-    interactions: InteractionSchema;
-    trendingScore?: number
-    createdAt?: Date
-    updatedAt?: Date
+	_id: Bson.ObjectId;
+	userId: Bson.ObjectId;
+	title: string;
+	content: string;
+	categories: string[];
+	interactions: InteractionSchema;
+	trendingScore?: number;
+	createdAt?: Date;
+	updatedAt?: Date;
 }
 
 export interface InteractionSchema {
-    likes: number
-    comments: number
-    shares: number
-    clicked: number
-    profileClicked: number
-    bookmarked: number
-    photoExpanded: number
-    videoPlayback: number
+	likes: number;
+	comments: number;
+	shares: number;
+	clicked: number;
+	profileClicked: number;
+	bookmarked: number;
+	photoExpanded: number;
+	videoPlayback: number;
 }
 
 let postsCollection: Collection<PostSchema> | null = null;
 
 export const getPostsCollection = async () => {
-    if (postsCollection) {
-        return postsCollection;
-    }
+	if (postsCollection) {
+		return postsCollection;
+	}
 
-    const db = await getDB();
-    postsCollection = db.collection<PostSchema>("post");
-    return postsCollection;
-}
+	const db = await getDB();
+	postsCollection = db.collection<PostSchema>("post");
+	return postsCollection;
+};
