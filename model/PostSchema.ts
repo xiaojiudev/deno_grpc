72fe74d4461feade4ec715d3473e68961f4c19c3
ldphat99 <ldphat99@gmail.com>
2024-06-10 11:39:38 +0700

    refactor: update some fields for post schema
    *add categories field
    *add interactions field
    *add trendingScore field

--- model/PostSchema.ts
+++ model/PostSchema.ts
@@ -1,15 +1,26 @@
-import { mongoose } from "../deps.ts";
-
-const Schema = mongoose.Schema;
-const ObjectId = Schema.ObjectId;
+import { ObjectId, Schema, mongoose } from "../deps.ts";
 
 export interface IPost {
     id?: mongoose.Types.ObjectId;
     user: mongoose.Types.ObjectId;
     title: string;
     content: string;
+    categories: string[];
+    interactions: IPostInteraction;
+    trendingScore?: number;
     createdAt?: Date;
     updatedAt?: Date;
+}
+
+export interface IPostInteraction {
+    likes: number;
+    comments: number;
+    shares: number;
+    clicked: number;
+    profileClicked: number;
+    bookmarked: number;
+    photoExpanded: number;
+    videoPlayback: number;
 }
 
 interface IPostMethods {
@@ -22,9 +33,21 @@ type PostModel = mongoose.Model<IPost, {}, IPostMethods>;
 
 const PostSchema = new Schema<IPost, PostModel, IPostMethods>({
     id: { type: ObjectId },
-    user: { type: mongoose.Types.ObjectId, ref: 'User', },
+    user: { type: mongoose.Types.ObjectId, ref: 'Users', },
     title: { type: String, required: true, },
     content: { type: String, required: true, },
+    categories: [String],
+    interactions: {
+        likes: { type: Number, default: 0 },
+        comments: { type: Number, default: 0 },
+        shares: { type: Number, default: 0 },
+        clicked: { type: Number, default: 0 },
+        profileClicked: { type: Number, default: 0 },
+        bookmarked: { type: Number, default: 0 },
+        photoExpanded: { type: Number, default: 0 },
+        videoPlayback: { type: Number, default: 0 },
+    },
+    trendingScore: { type: Number, default: 0},
     createdAt: { type: Date, default: Date.now },
     updatedAt: { type: Date, default: Date.now },
 });
@@ -46,5 +69,5 @@ PostSchema.set('toJSON', {
 
 PostSchema.set('toObject', { virtuals: true })
 
-export const PostCollection = mongoose.model('Post', PostSchema);
+export const PostCollection = mongoose.model('Posts', PostSchema);
 
