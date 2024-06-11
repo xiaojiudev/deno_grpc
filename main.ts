0eb538b106a3f532430df31eaeb42a5454c3cfe5
ldphat99 <ldphat99@gmail.com>
2024-06-11 17:07:21 +0700

    refactor: add cronjob, mock data

--- main.ts
+++ main.ts
@@ -1,15 +1,19 @@
 import { APP_GRPC_PORT } from "./deps.ts";
 import { connectDB } from "./db/mongodb.ts";
-import { getGrpcServer, initGRPCService } from "./db/grpc.ts";
-import { connectEs } from "./db/elasticsearch.ts";
+import { getGrpcServer, initGRPCService } from "./grpc/grpc.ts";
+import { connectEs, deleteIndex } from "./db/elasticsearch.ts";
+import { getMockPostData } from "./utils/mockDB.ts";
+import { updateTrendingScore } from "./services/TrendingService.ts";
+import { appCronJob } from "./services/CronJobService.ts";
 
 const startServer = async (): Promise<void> => {
 	try {
 		await connectDB();
 		await connectEs();
+		await getMockPostData();
 		const grpcServer = getGrpcServer();
 		await initGRPCService();
-
+		appCronJob();
 		console.log(`gRPC server gonna listen on ${APP_GRPC_PORT} port`);
 
 		for await (const conn of Deno.listen({ port: APP_GRPC_PORT })) {
