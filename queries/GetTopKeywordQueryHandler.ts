581228922e6c27cc7be8bb4090386a97582f19c5
ldphat99 <ldphat99@gmail.com>
2024-06-11 17:11:12 +0700

    refactor: use aggregation instead of query

--- queries/GetTopKeywordQueryHandler.ts
+++ queries/GetTopKeywordQueryHandler.ts
@@ -1,81 +1,36 @@
-import { queryEs } from "../db/elasticsearch.ts";
+import { getEs } from "../db/elasticsearch.ts";
 import { Empty, KeywordList } from "../deps.ts";
-import { WordBagSchema } from "../model/WordBagSchema.ts";
-
-export interface WordBagEs extends WordBagSchema {
-	id: string;
-}
 
 export class GetTopKeywordQueryHandler {
 	async handle(_query: Empty): Promise<KeywordList> {
-		const currentDate = new Date().toISOString().split("T")[0];
+		const esClient = getEs();
 
-		const wordbagData = await queryEs({
-			index: "wordbag",
-			_source: true,
-			size: 10,
-			query: {
-				function_score: {
-					score_mode: "multiply", // scores are multiplied
-					boost_mode: "multiply", // query score and function score is multiplied 
-					functions: [
-						{
-							gauss: {
-								"dateCount.date": {
-									origin: 'now/d',
-									scale: '3d',
-									decay: 0.5,
-								}
-							},
-						},
-					],
-					query: {
-						bool: {
-							should: [
-								{
-									term: {
-										"dateCount.date": 'now/d',
-									},
-								},
-								{
-									range: {
-										"dateCount.date": {
-											gte: "now-7d/d",
-											lte: "now/d"
-										}
-									},
-								},
-							],
-						},
 
+		const aggregationData = await esClient.search({
+			index: "queries",
+			_source: false,
+			aggs: {
+				"top-query": {
+					terms: {
+						field: "queryString.keyword", // Use .keyword to aggregate on the exact value
+						size: 10,
 					},
-				}
-			},
-			sort: [{
-				"_score": { order: "desc" }
-			},
-			{
-				"dateCount.count": {
-					order: "desc",
-					mode: "avg",
+					// aggs: {
+					// 	"top-query-hits": {
+					// 		top_hits: {
+					// 			highlight: {
+					// 				fields: {
+					// 					"queryString": {}
+					// 				}
+					// 			}
+					// 		}
+					// 	}
+					// },
+					
 				},
-			},
-			],
+			}
 		});
-
-		console.log(wordbagData);
-
-
-		if (wordbagData) {
-			// deno-lint-ignore no-explicit-any
-			const mappedData = wordbagData.map((wordEs: any) => {
-				const word = wordEs._source as WordBagEs;
-
-				return word.word;
-			});
-
-			return { keywords: [...mappedData] };
-		}
+		console.log(aggregationData);
 
 		return { keywords: [] };
 	}
