f2a487d344bbd12f63578f172d87bc03ea24b7e5
ldphat99 <ldphat99@gmail.com>
2024-06-12 11:25:08 +0700

    refactor: updated get top keyword query
    - Update get top keyword query by query within a range of one day, then use aggregation to extract top 10 by queryString field

--- queries/GetTopKeywordQueryHandler.ts
+++ queries/GetTopKeywordQueryHandler.ts
@@ -1,6 +1,8 @@
 import { getEs } from "../db/elasticsearch.ts";
 import { Empty, KeywordList } from "../deps.ts";
 
+
+
 export class GetTopKeywordQueryHandler {
 	async handle(_query: Empty): Promise<KeywordList> {
 		const esClient = getEs();
@@ -9,29 +11,42 @@ export class GetTopKeywordQueryHandler {
 		const aggregationData = await esClient.search({
 			index: "queries",
 			_source: false,
+			size: 0, // needn't search hits, just show the results
+			query: {
+				range: {
+					"queryDate": { // query data within a range of one day
+						gte: "now-1d/d",
+						lte: "now/d"
+					}
+				}
+			},
 			aggs: {
-				"top-query": {
+				"top_query": {
 					terms: {
 						field: "queryString.keyword", // Use .keyword to aggregate on the exact value
 						size: 10,
 					},
-					// aggs: {
-					// 	"top-query-hits": {
-					// 		top_hits: {
-					// 			highlight: {
-					// 				fields: {
-					// 					"queryString": {}
-					// 				}
-					// 			}
-					// 		}
-					// 	}
-					// },
-					
+					aggs: {
+						"sort_by_count": {
+							bucket_sort: {
+								sort: [
+									{ "_count": { order: "desc" } }
+								]
+							}
+						}
+					}
 				},
-			}
+			},
 		});
-		console.log(aggregationData);
+		// console.log(aggregationData);
 
-		return { keywords: [] };
+		// @ts-ignore: Elasticsearch types don't fully capture aggregation structure
+		const buckets = aggregationData.aggregations?.top_query.buckets;
+		console.log(buckets);
+		
+
+		const topKeywords = buckets.map((item: { key: string; doc_count: number }) => item.key);
+
+		return { keywords: [...topKeywords] };
 	}
 }
