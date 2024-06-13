d6affc912e526f34aa0caacb479f08b71cd79c9c
ldphat99 <ldphat99@gmail.com>
2024-06-13 15:13:51 +0700

    feat: add centralized exports for all query handlers

---
+++ queries/index.ts
@@ -0,0 +1,15 @@
+import { GetPostQueryHandler } from "./GetPostQueryHandler.ts";
+import { GetPostsQueryHandler } from "./GetPostsQueryHandler.ts";
+import { GetTrendingPostsQueryHandler } from "./GetTrendingPostsQueryHandler.ts";
+import { SearchPostQueryHandler } from "./SearchPostQueryHandler.ts";
+import { GetTopKeywordQueryHandler } from "./GetTopKeywordQueryHandler.ts";
+import { RecommendPostsQueryHandler } from "./RecommendPostsQueryHandler.ts";
+
+export {
+	GetPostQueryHandler,
+	GetPostsQueryHandler,
+	GetTopKeywordQueryHandler,
+	GetTrendingPostsQueryHandler,
+	RecommendPostsQueryHandler,
+	SearchPostQueryHandler,
+};
