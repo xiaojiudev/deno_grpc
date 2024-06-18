f164692c29346d7ee9bfe1635794e3bf23bb1abb
ldphat99 <ldphat99@gmail.com>
2024-06-18 08:48:29 +0700

    refactor: define args path

--- utils/pythonScript.ts
+++ utils/pythonScript.ts
@@ -1,9 +1,12 @@
 const PYTHON3_ENV_PATH = "C:/Program Files/Python36/python.exe";
 const FILE_EXEC_PATH = "./apply_collaborative_filtering.py";
+const DATASET_PATH = "./test.data";
+const DEFAULT_RECOMMEND_TOP_N = "3";
 const USER_ID = "3";
 
+
 const command = new Deno.Command(PYTHON3_ENV_PATH, {
-    args: [FILE_EXEC_PATH, USER_ID],
+    args: [FILE_EXEC_PATH, USER_ID, DATASET_PATH, DEFAULT_RECOMMEND_TOP_N],
 });
 const { code, stdout, stderr } = await command.output();
 console.log(new TextDecoder().decode(stdout));
