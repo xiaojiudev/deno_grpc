f6d3e3c7a973deec24147d885a29dd05d92af314
ldphat99 <ldphat99@gmail.com>
2024-06-19 11:26:56 +0700

    feat: conjob for writing user favorite log

--- services/CronJobService.ts
+++ services/CronJobService.ts
@@ -1,8 +1,14 @@
+import { saveUserFavLog } from "./LogService.ts";
 import { updateTrendingScore } from "./TrendingService.ts";
 
 export const appCronJob = (): void => {
 	Deno.cron("Update trending scores", { minute: { every: 1 } }, async () => {
 		console.log("Updating trending scores...");
 		await updateTrendingScore();
+	});
+
+	Deno.cron("Write user fav log", { minute: { every: 1 } }, async () => {
+		console.log("Write user fav log");
+		await saveUserFavLog();
 	});
 };
