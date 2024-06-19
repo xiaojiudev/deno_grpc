9aac4796e522c86cf37939f2868f3d388dcf864d
ldphat99 <ldphat99@gmail.com>
2024-06-19 11:19:45 +0700

    feat: write user favorite into log file

---
+++ services/LogService.ts
@@ -0,0 +1,32 @@
+import { UserCollection } from "../model/UserSchema.ts";
+import { CSVWriter } from "jsr:@vslinko/csv@1.0.2";
+
+export const saveUserFavLog = async () => {
+    const userDocs = await UserCollection.find({});
+
+    const f = await Deno.open("./user_fav.data", {
+        write: true,
+        create: true,
+        truncate: true,
+    });
+
+    const writer = new CSVWriter(f, {
+        columnSeparator: "\t",
+        lineSeparator: "\r\n",
+    });
+
+    for (const user of userDocs) {
+        const { id, favCategories } = user.toClient();
+        if (id && favCategories) {
+            for (const category of favCategories) {
+                await writer.writeCell(id.toString());
+                await writer.writeCell(category.toString());
+                await writer.writeCell("1");
+                await writer.writeCell(Date.now().toString());
+                await writer.nextLine();
+            }
+        }
+    }
+
+    f.close();
+}
