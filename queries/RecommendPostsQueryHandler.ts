67b8bf848bf47bf685c5459ff9552400d24e458a
ldphat99 <ldphat99@gmail.com>
2024-06-19 11:28:04 +0700

    feat: recommend post function
    *temporary code, not optimized yet

--- queries/RecommendPostsQueryHandler.ts
+++ queries/RecommendPostsQueryHandler.ts
@@ -1,89 +1,55 @@
 import { POST_INDEX } from "../constant/index.ts";
 import { queryEs } from "../db/elasticsearch.ts";
-import { ObjectId } from "../deps.ts";
+import { ObjectId, mongoose } from "../deps.ts";
+import { Post } from "../deps.ts";
 import { PostList, UserRequest } from "../deps.ts";
+import { CategoryCollection } from "../model/CategorySchema.ts";
+import { PostCollection } from "../model/PostSchema.ts";
+import { UserCollection } from "../model/UserSchema.ts";
+import { RequestAgrs, getRecommendationPosts } from "../utils/pythonScript.ts";
+
 
 export class RecommendPostsQueryHandler {
 	async handle(request: UserRequest): Promise<PostList> {
 		const userId = request.userId;
 
-		// if (!userId || !ObjectId.isValid(userId)) {
-		// 	return { posts: [] };
-		// }
+		const categoryDocs = await CategoryCollection.find({}).distinct('_id');
+		const CATEGORY_LIST = categoryDocs.map((c) => c._id.toString());
 
-		// const UserCollection = await getUsersCollection();
-		// const user = await UserCollection.findOne({
-		// 	_id: new ObjectId(userId),
-		// });
-		// if (!user) {
-		// 	return { posts: [] };
-		// }
+		const payload: RequestAgrs = {
+			DATASET_PATH: "./user_fav.data",
+			CATEGORY_LIST: CATEGORY_LIST,
+			USER_ID: userId,
+			RECOMMEND_TOP_N: 5
+		}
+		const res = await getRecommendationPosts(payload);
 
-		// const userCategories = user.favoriteCategories;
-		// // console.log(userCategories);
+		console.log(res);
+		const cateIds = res.map((c) => new mongoose.Types.ObjectId(c));
 
-		// const recommendPosts: PostES[] = [];
+		const test = await PostCollection.find({}, {
+			_id: 1,
+			user: 1,
+			title: 1,
+			content: 1,
+			trendingScore: 1,
+		})
+			.where("categories")
+			.in(cateIds)
+			.sort([["trendingScore", -1]])
+			.exec();
 
-		// for (const category of userCategories) {
-		// 	const postsQueryRes = await queryEs({
-		// 		index: POST_INDEX,
-		// 		size: 10,
-		// 		query: {
-		// 			match_phrase: {
-		// 				categories: category,
-		// 			},
-		// 		},
-		// 		sort: [
-		// 			{ "trendingScore": "desc" },
-		// 		],
-		// 	});
-		// 	// console.log(postsQueryRes);
+		console.log(test);
 
-		// 	if (postsQueryRes) {
-		// 		postsQueryRes.forEach((item) => {
-		// 			const postData = item._source as PostES;
+		const postRes = test.map(t => {
+			const temp: Post = {
+				userId: t._id.toString(),
+				title: t.title,
+				content: t.content
+			}
+			return temp;
+		});
 
-		// 			recommendPosts.push({ ...postData });
-		// 		});
-		// 	}
-		// }
-
-		// // console.log(recommendPosts);
-
-		// const map = new Map<string, number>();
-
-		// for (const post of recommendPosts) {
-		// 	const postId = post.id;
-		// 	map.set(postId, (map.get(postId) ?? 0) + 1);
-		// }
-
-		// const top10PostIds = Array.from(map.entries())
-		// 	.sort((a, b) => b[1] - a[1])
-		// 	.slice(0, 10)
-		// 	.map(([postId]) => postId);
-
-		// const top10Posts: PostES[] = [];
-
-		// for (const postId of top10PostIds) {
-		// 	const postQueryRes = await queryEs({
-		// 		index: POST_INDEX,
-		// 		size: 1,
-		// 		query: {
-		// 			term: { _id: postId },
-		// 		},
-		// 	});
-		// 	if (postQueryRes && postQueryRes.length > 0) {
-		// 		const postData = postQueryRes[0]._source as PostES;
-		// 		top10Posts.push(postData);
-		// 	}
-		// }
-
-		// top10Posts.sort((a, b) => b.trendingScore! - a.trendingScore!);
-
-		// console.log(top10PostIds);
-
-		// return { posts: [...top10Posts] };
-
-		return { posts: [] };
+		return { posts: [...postRes] };
 	}
 }
