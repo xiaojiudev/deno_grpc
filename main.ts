d4c198c1f7fbb3c37704657dc005cb503e8910a3
ldphat99 <ldphat99@gmail.com>
2024-06-20 10:40:26 +0700

    refactor: optimize to init grpc server

--- main.ts
+++ main.ts
@@ -1,6 +1,6 @@
 import { APP_GRPC_PORT } from "./deps.ts";
 import { connectDB } from "./db/mongodb.ts";
-import { getGrpcServer, initGRPCService } from "./grpc/grpc.ts";
+import { initGRPCService } from "./grpc/grpc.ts";
 import { connectEs } from "./db/elasticsearch.ts";
 import { getMockPostData } from "./utils/mockDB.ts";
 import { appCronJob } from "./services/CronJobService.ts";
@@ -9,14 +9,13 @@ const startServer = async (): Promise<void> => {
 	try {
 		await connectDB();
 		await connectEs();
+		const grpcInstance = await initGRPCService();
 		await getMockPostData();
-		await initGRPCService();
-		const grpcServer = getGrpcServer();
 		appCronJob();
 		console.log(`gRPC server gonna listen on - port: ${APP_GRPC_PORT} `);
 
 		for await (const conn of Deno.listen({ port: APP_GRPC_PORT })) {
-			grpcServer.handle(conn);
+			grpcInstance.handle(conn);
 		}
 	} catch (error) {
 		console.log("Start server failed!", error);
