f2155427ef47fe4cd9e84699b05c2653ce95894b
ldphat99 <ldphat99@gmail.com>
2024-06-20 10:30:47 +0700

    chore: optimize function
    *throw err when an exception occur
    *format log style

--- db/mongodb.ts
+++ db/mongodb.ts
@@ -1,17 +1,17 @@
 import { APP_DATABASE_NAME, APP_DATABASE_URI, mongoose } from "../deps.ts";
 
-export const connectDB = async (): Promise<void> => {
 const configOptions = {
 	dbName: APP_DATABASE_NAME,
 } as mongoose.ConnectOptions;
 
-	await mongoose.connect(APP_DATABASE_URI, options as mongoose.ConnectOptions)
+export const connectDB = async (): Promise<void> => {
+	await mongoose
+		.connect(APP_DATABASE_URI, configOptions)
 		.then((info) => {
-			console.log(
-				`Connected to Mongo Database successfully - port: ${info.connection.port}`,
-			);
+			console.log(`Connected to Mongo Database successfully - port: ${info.connection.port}`);
 		})
 		.catch((err) => {
-			console.log("Not Connected to Database ERROR! ", err);
+			console.log("Connected to Mongo Database ERROR! ", err);
+			throw err;
 		});
 };
