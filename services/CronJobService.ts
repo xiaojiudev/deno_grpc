79dea9f012ebb54aa4de48becc6785487c4ed884
ldphat99 <ldphat99@gmail.com>
2024-06-20 10:37:26 +0700

    refactor: handle CronJob exception
    *add try/catch block

--- services/CronJobService.ts
+++ services/CronJobService.ts
@@ -2,13 +2,18 @@ import { saveUserFavLog } from "./LogService.ts";
 import { updateTrendingScore } from "./TrendingService.ts";
 
 export const appCronJob = (): void => {
-	Deno.cron("Update trending scores", { minute: { every: 1 } }, async () => {
-		console.log("Updating trending scores...");
-		await updateTrendingScore();
-	});
+	try {
+		Deno.cron("Update trending scores", { minute: { every: 1 } }, async () => {
+			console.log("Updating trending scores...");
+			await updateTrendingScore();
+		});
 
-	Deno.cron("Write user fav log", { minute: { every: 1 } }, async () => {
-		console.log("Write user fav log");
-		await saveUserFavLog();
-	});
+		Deno.cron("Write user fav log", { minute: { every: 1 } }, async () => {
+			console.log("Write user fav log");
+			await saveUserFavLog();
+		});
+	} catch (error) {
+		console.log("Setup app CronJob ERROR");
+		throw error;
+	}
 };
