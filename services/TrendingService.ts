ef1d69a1ae493c6495f056164cfdb6e854685228
ldphat99 <ldphat99@gmail.com>
2024-06-20 10:38:50 +0700

    style: format code style

--- services/TrendingService.ts
+++ services/TrendingService.ts
@@ -21,27 +21,29 @@ const sigmoid = (x: number): number => {
 
 const calculateTrendingScrore = (post: IPost): number => {
 	const currentTime = new Date().getTime();
-	const postTime = post.createdAt?.getTime() || 0;
-
-	const timeDifference = (currentTime - postTime) / (1000 * 60 * 60 * 24);
+	const postedTime = post.createdAt?.getTime() || 0;
+	const timeDifference = (currentTime - postedTime) / (1000 * 60 * 60 * 24);
 
 	let interactionScore = 0;
 
-	interactionScore += post.interactions.clicked *
-		EVENT_TYPE_STRENGTH.IS_CLICKED;
-	interactionScore += post.interactions.profileClicked *
-		EVENT_TYPE_STRENGTH.IS_PROFILE_CLICKED;
-	interactionScore += post.interactions.likes * EVENT_TYPE_STRENGTH.IS_LIKED;
-	interactionScore += post.interactions.comments *
-		EVENT_TYPE_STRENGTH.IS_COMMENTED;
-	interactionScore += post.interactions.shares *
-		EVENT_TYPE_STRENGTH.IS_SHARED;
-	interactionScore += post.interactions.bookmarked *
-		EVENT_TYPE_STRENGTH.IS_BOOKMARKED;
-	interactionScore += post.interactions.photoExpanded *
-		EVENT_TYPE_STRENGTH.IS_PHOTO_EXPANDED;
-	interactionScore += post.interactions.videoPlayback *
-		EVENT_TYPE_STRENGTH.IS_VIDEO_PLAYBACK;
+	if (post?.interactions) {
+		interactionScore += post.interactions.clicked *
+			EVENT_TYPE_STRENGTH.IS_CLICKED;
+		interactionScore += post.interactions.profileClicked *
+			EVENT_TYPE_STRENGTH.IS_PROFILE_CLICKED;
+		interactionScore += post.interactions.likes *
+			EVENT_TYPE_STRENGTH.IS_LIKED;
+		interactionScore += post.interactions.comments *
+			EVENT_TYPE_STRENGTH.IS_COMMENTED;
+		interactionScore += post.interactions.shares *
+			EVENT_TYPE_STRENGTH.IS_SHARED;
+		interactionScore += post.interactions.bookmarked *
+			EVENT_TYPE_STRENGTH.IS_BOOKMARKED;
+		interactionScore += post.interactions.photoExpanded *
+			EVENT_TYPE_STRENGTH.IS_PHOTO_EXPANDED;
+		interactionScore += post.interactions.videoPlayback *
+			EVENT_TYPE_STRENGTH.IS_VIDEO_PLAYBACK;
+	}
 
 	const trendingScore = interactionScore *
 		Math.exp(-DECAY_RATE * timeDifference);
@@ -56,16 +58,17 @@ export const updateTrendingScore = async (): Promise<void> => {
 		const trendingScore = calculateTrendingScrore(post);
 		console.log(`Post ${index + 1} - ${trendingScore}`);
 
-		const res = await PostCollection.findOneAndUpdate(
+		const docRes = await PostCollection.findOneAndUpdate(
 			{ _id: post._id },
 			{ trendingScore: trendingScore },
 			{ includeResultMetadata: true },
 		);
-		const postData = res.value;
+
+		const postData = docRes.value;
 
 		if (postData) {
-			const mappedPostData = postData.toClient();
 			const esClient = getEs();
+			const mappedPostData = postData.toClient();
 
 			await esClient.update({
 				index: POST_INDEX,
