44a5264c1f017291355d853aa3a3e6d309b2592f
ldphat99 <ldphat99@gmail.com>
2024-06-28 09:30:23 +0700

    refactor: remove unused import

--- deps.ts
+++ deps.ts
@@ -5,33 +5,11 @@ export { GrpcServer } from "https://deno.land/x/grpc_basic@0.4.7/server.ts";
 export { getClient } from "https://deno.land/x/grpc_basic@0.4.7/client.ts";
 
 export {
-	error as GrpcException,
-	GrpcError,
-	Status as GrpcStatus,
-} from "https://deno.land/x/grpc_basic@0.4.7/error.ts";
-
-export {
 	Database,
 	MongoClient,
 } from "https://deno.land/x/mongo@v0.32.0/mod.ts";
 
 export * as bcrypt from "https://deno.land/x/bcrypt@v0.4.1/mod.ts";
-
-export {
-	afterEach,
-	beforeEach,
-	describe,
-	it,
-} from "https://deno.land/std@0.224.0/testing/bdd.ts";
-
-export {
-	assertArrayIncludes,
-	assertRejects,
-} from "https://deno.land/std@0.224.0/testing/asserts.ts";
-
-export { assertEquals } from "https://deno.land/std@0.224.0/assert/assert_equals.ts";
-
-export { assertObjectMatch } from "https://deno.land/std@0.224.0/assert/assert_object_match.ts";
 
 export * from "./types/social_media.d.ts";
 
