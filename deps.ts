33efde6c3ba31c5607c795da8f276687ef3a4119
ldphat99 <ldphat99@gmail.com>
2024-06-28 09:57:25 +0700

    style: style fmt

--- deps.ts
+++ deps.ts
@@ -4,10 +4,7 @@ export { GrpcServer } from "https://deno.land/x/grpc_basic@0.4.7/server.ts";
 
 export { getClient } from "https://deno.land/x/grpc_basic@0.4.7/client.ts";
 
-export {
-	Database,
-	MongoClient,
-} from "https://deno.land/x/mongo@v0.32.0/mod.ts";
+export { Database, MongoClient } from "https://deno.land/x/mongo@v0.32.0/mod.ts";
 
 export * as bcrypt from "https://deno.land/x/bcrypt@v0.4.1/mod.ts";
 
@@ -32,10 +29,10 @@ export const validObjectId = (id: string): boolean => {
 };
 
 export const generateObjectId = (id?: string): ObjectIdType => {
-	if(id && validObjectId(id)) {
+	if (id && validObjectId(id)) {
 		// @ts-ignore mongoose ObjectId typescript bug
 		return new mongoose.Types.ObjectId(id);
 	} else {
 		return new mongoose.Types.ObjectId();
 	}
-}
+};
